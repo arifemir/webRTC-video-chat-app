c7f0800620484a73aa23eee1fe7df44f6c070821
arifemir <arifarslanturk@gmail.com>
2020-12-26 21:19:58 +0300

    -join component css re write;

--- client/src/pages/join.js
+++ client/src/pages/join.js
@@ -1,18 +1,23 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
+import './Join.css'
 
 const Join = () => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
   return (
-    <div className='join-container'>
-      <div className="join-inner-container">
-        <h1 className="header"></h1>
-        <div><input placeholder='Name' type="text" className="join-input" onChange={(e) => setName(e.target.value)} value={name}/></div>
-        <div><input placeholder='Room' type="text" className="join-input" onChange={(e) => setRoom(e.target.value)} value={room}/></div>
-        <Link onClick={e => (!name || !room) && e.preventDefault()} to={`/chat?name=${name}&room=${room}`}>
-          <button className="button" type='submit'>Sign in</button>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div>
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+        </div>
+        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className={'button mt-20'} type="submit">Sign In</button>
         </Link>
       </div>
     </div>
